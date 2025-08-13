f4cda956b56cf6db6a8227e6577d106fdd582b6c
Gabriel <gabrieldejesusfc.12@gmail.com>
2025-08-13 11:48:48 -0300

    update: validation.js

--- utils/validation.js
+++ utils/validation.js
@@ -5,17 +5,14 @@ export default (Schema) => {
         try {
             const { error, value } = Schema.validate(req.body)
 
-            if (!error) {
-                req.body = value 
-                return next()   
-            } 
+            if (error === undefined || typeof error === 'undefined') return next()
             
             const err = new Error(
                 error.details.map((errorObject) => errorObject.message).toString()
             )
+            
             err.statusCode = 422
             next(err)
-
         } catch (err) {
             next(err)
         }
