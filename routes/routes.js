7fa98017fcef95899c063fb9984a7123ce00df22
Gabriel <gabrieldejesusfc.12@gmail.com>
2025-08-13 11:40:02 -0300

    feat: creating middleware to data validation

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const controllers = require('../controllers')
+const validate = require('../utils/validation')
+const { user, to_do } = require('../models')
 
 router.get('/', controllers)
-
+// router.post('/register', validate(user), controllers)
 
 
 module.exports = router
---
+++ utils/validation.js
@@ -0,0 +1,23 @@
+export default (Schema) => {
+    return (req, res, next) => {
+
+        // validacao 
+        try {
+            const { error, value } = Schema.validate(req.body)
+
+            if (!error) {
+                req.body = value 
+                return next()   
+            } 
+            
+            const err = new Error(
+                error.details.map((errorObject) => errorObject.message).toString()
+            )
+            err.statusCode = 422
+            next(err)
+
+        } catch (err) {
+            next(err)
+        }
+    }
+}
